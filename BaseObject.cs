fc4d4641af248d0d47ed1b5547fd27d168a46e9d
VovaMaybeNextTime <48494728+VovaMaybeNextTime@users.noreply.github.com>
2021-04-07 21:06:06 +0300

    Add files via upload

---
+++ BaseObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace lab3
+{
+    public class BaseObject
+    {
+        public static IWebDriver Driver;
+        public static WebDriverBackedSelenium Selenium;
+
+        public static void InitPage<T>(T pageClass) where T : BaseObject
+        {
+            PageFactory.InitElements(Driver, pageClass);
+        }
+    }
+}
---
+++ BaseTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3.Tests
+{
+    class BaseTest : BaseObject
+    {
+        //public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void beforeClass()
+        {
+            Driver = new ChromeDriver("/Users/vladimir/Desktop/kpi3/res");
+            //Selenium = new DefaultSelenium()
+            //Selenium.Start();
+            //Selenium.WindowMaximize();
+        }
+
+        [SetUp]
+        public void beforeTest()
+        {
+            Driver.Navigate().GoToUrl(Utils.baseUrl);
+        }
+
+        [OneTimeTearDown]
+        public void afterClass()
+        {
+            Driver.Close();
+           // Selenium.Close();
+        }
+
+
+    }
+}
